2f91e5783d7751d8402316337f80134f54e10e01
Santosh Shrestha <sthasantosh070@gmail.com>
2025-01-26 21:37:04 +0545

    Changes done on backend part

--- Backend/controller/userController.js
+++ Backend/controller/userController.js
@@ -1,1 +1,35 @@
-const Users = require()
+const Users = require("../model/userSchema");
+ 
+const getAllCustomer = async (req, res) => {
+  console.log("Get All Customer");
+  try {
+    const users = await Users.findAll();
+ 
+    if (users.length === 0) {
+      res
+        .status(200)
+        .send({ data: users, message: "successfully fetched data" });
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json("Error While fetching");
+  }
+};
+ 
+const saveAllCustomer = async (req, res) => {
+  console.log(req.body);
+  const { name, userId ,email , password } = req.body;
+  try {
+    const user = await Users.findOne({ where: { userId: userId } });
+ 
+    if (user == null) {
+      await Users.create(req.body);
+      return res.status(201).json({ message: "Users Added Sucessfully" });
+    }
+    return res.status(500).json({ message: "user is already Present" });
+  } catch (error) {
+    console.log(error);
+  }
+};
+ 
+module.exports = { getAllCustomer, saveAllCustomer };
--- Backend/routes/userRoute.js
+++ Backend/routes/userRoute.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const{
     getAllCustomer,
+    saveAllCustomer,
 }=require("../controller/userController");
 
 const router = express.Router();
 
 router.get('/users',getAllCustomer);
+router.post("/users",saveAllCustomer)
 
 module.exports = {router};
